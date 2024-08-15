d6ce829013c586ce12b8eb9651e889f8739df020
Felix Zimmermann <felix.zimmermann.1.de@gmail.com>
2024-08-15 20:49:16 +0200

    persistent index; multi-lang embeddings

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import warnings
-warnings.filterwarnings("ignore", message=".*Torch was not compiled with flash attention.*")
-
-import time
-from llama_index.llms.ollama import Ollama
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 import torch
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings, StorageContext, load_index_from_storage
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.ollama import Ollama
+import time
+import os.path
+import warnings
+
+warnings.filterwarnings(
+    "ignore", message=".*Torch was not compiled with flash attention.*")
+
 
 # Start time
 start_time = time.time()
@@ -18,13 +21,22 @@ print(f"Using device: {device}")
 documents = SimpleDirectoryReader("data").load_data()
 
 # Embeddings model
-Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-m3")
 
 # Language model
 Settings.llm = Ollama(model="llama3.1", request_timeout=360.0, device=device)
 
-# Create index
-index = VectorStoreIndex.from_documents(documents)
+PERSIST_DIR = "./storage"
+if not os.path.exists(PERSIST_DIR):
+    # load the documents and create the index
+    documents = SimpleDirectoryReader("data").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    # load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
 
 # Perform RAG query
 query_engine = index.as_query_engine()
