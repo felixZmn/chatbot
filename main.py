01bac3b743529d2046a1e39236b117ed1df4f80d
Felix Zimmermann <felix.zimmermann.1.de@gmail.com>
2024-08-16 09:46:10 +0200

    enabled adding of files

--- main.py
+++ main.py
@@ -3,50 +3,65 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings, 
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
 import time
-import os.path
 import warnings
 
 warnings.filterwarnings(
     "ignore", message=".*Torch was not compiled with flash attention.*")
 
-
-# Start time
-start_time = time.time()
-
-# Check if CUDA is available
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using device: {device}")
-
-# My local documents
-documents = SimpleDirectoryReader("data").load_data()
-
-# Embeddings model
-Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-m3")
-
-# Language model
-Settings.llm = Ollama(model="llama3.1", request_timeout=360.0, device=device)
-
 PERSIST_DIR = "./storage"
-if not os.path.exists(PERSIST_DIR):
-    # load the documents and create the index
-    documents = SimpleDirectoryReader("data").load_data()
-    index = VectorStoreIndex.from_documents(documents)
-    # store it for later
+
+
+def load_index(directory):
+    documents = SimpleDirectoryReader(
+        directory, filename_as_id=True).load_data()
+
+    try:
+        # Load index from storage
+        print("Loading index from storage...")
+        storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+        index = load_index_from_storage(storage_context)
+    except FileNotFoundError:
+        # Create index from documents and persist it
+        print("Creating index from documents...")
+        index = VectorStoreIndex.from_documents(documents)
+        index.storage_context.persist(persist_dir=PERSIST_DIR)
+
+    # Refresh the reference documents and persist the index
+    print("Refreshing reference documents...")
+    index.refresh_ref_docs(
+        documents, update_kwargs={"delete_kwargs": {'delete_from_docstore': True}})
     index.storage_context.persist(persist_dir=PERSIST_DIR)
-else:
-    # load the existing index
-    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
-    index = load_index_from_storage(storage_context)
+    return index
 
-# Perform RAG query
-query_engine = index.as_query_engine()
-response = query_engine.query(
-    "Was ist zur Abgabe der Bachelorarbeit notwendig?")
-print(response)
 
-# End time
-end_time = time.time()
+if __name__ == "__main__":
+    # Start time
+    start_time = time.time()
 
-# Calculate and print the elapsed time
-elapsed_time = end_time - start_time
-print(f"Elapsed time: {elapsed_time:.2f} seconds")
+    # Check if CUDA is available
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using device: {device}")
+
+    # Embeddings model
+    Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-m3")
+
+    # Language model
+    Settings.llm = Ollama(
+        model="llama3.1", request_timeout=360.0, device=device)
+
+    # Load index with documents from ./data
+    index = load_index("data")
+
+    # Perform RAG query
+    print("Performing query...")
+    query_engine = index.as_query_engine()
+    response = query_engine.query(
+        "Was ist zur Abgabe der Bachelorarbeit notwendig?")
+    print(response)
+
+    # End time
+    end_time = time.time()
+
+    # Calculate and print the elapsed time
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")
