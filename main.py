286683b19a4fa7cbe2ab50140f73056d8d492fda
mike230419 <mikewaldenmaier@yahoo.com>
2024-08-27 17:43:59 +0200

    fix printing response in chat-loop

--- main.py
+++ main.py
@@ -46,5 +46,5 @@ if __name__ == "__main__":
 
     # Loop for chat
     while True:
-        chat_bot.perform_query(input("\nFrage: "), course)
+        print(chat_bot.perform_query(input("\nFrage: "), course))
         time.sleep(1)
