bd9b9af046a0113efa406411cd5c30d997a6c809
Jeremias <jeremias.stenzenberger@outlook.de>
2024-09-08 14:22:18 +0200

    fix rag call

---
+++ helpers/RagPrompt.py
@@ -0,0 +1,35 @@
+from llama_cloud import ChatMessage, MessageRole
+from llama_index.core import ChatPromptTemplate
+
+additional_kwargs = {}
+
+rag_messages = [
+    ChatMessage(
+        id=1,
+        index=1,
+        role=MessageRole.SYSTEM,
+        content=(
+            """
+                Beantworte die Frage anhand der gegebenen RAG Dokumente
+            """
+        ),
+        additional_kwargs=additional_kwargs
+    ),
+    ChatMessage(
+        id="user",
+        index=1,
+        role=MessageRole.USER,
+        content=(
+            """
+            Kontext-Informationen:
+            {context_str}
+            Frage:
+            {query_str}
+            Answer:
+            """
+        ),
+        additional_kwargs=additional_kwargs
+    )
+]
+
+rag_template = ChatPromptTemplate(rag_messages)
