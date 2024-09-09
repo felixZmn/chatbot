8d55caed36037f69fd0a4488f87d13296cf5737b
Felix Zimmermann <felix.zimmermann.1.de+github@gmail.com>
2024-09-09 10:30:52 +0200

    implemented log folder creation

--- ChatBot.py
+++ ChatBot.py
@@ -195,5 +195,5 @@ class ChatBot(object):
 
         response = query_engine.query(query)
         message_logger.info(
-            f"Course: {course} \t Query: {query} \t #response: {response}")
+            f"Course: {course} \t Query: {query} \t response: {response}")
         return response
--- logger.py
+++ logger.py
@@ -1,18 +1,25 @@
 import logging
+import os
 
 formatter = logging.Formatter(
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
-def chatbot_logger(logLevel: int = logging.WARNING, file: str = "temp/chatbot.log") -> logging.Logger:
+def _logger(logger: str, logLevel: int, file: str) -> logging.Logger:
+    if not os.path.exists(os.path.dirname(file)):
+        os.makedirs(os.path.dirname(file))
     handler = logging.FileHandler(
         filename=file, mode='a', encoding='utf-8')
     handler.setFormatter(formatter)
 
-    logger = logging.getLogger('ChatBot')
+    logger = logging.getLogger(logger)
     logger.setLevel(logLevel)
     logger.addHandler(handler)
     return logger
+
+
+def chatbot_logger(logLevel: int = logging.WARNING, file: str = "temp/chatbot.log") -> logging.Logger:
+    return _logger('ChatBot', logLevel, file)
 
 
 def message_logger(logLevel: int = logging.DEBUG, file: str = "temp/messages.log") -> logging.Logger:
@@ -20,11 +27,4 @@ def message_logger(logLevel: int = logging.DEBUG, file: str = "temp/messages.log
     message logging enabled: logLevel = logging.INFO or logging.DEBUG
     message logging disabled: logLevel = logging.WARNING, logging.ERROR, logging.CRITICAL
     """
-    handler = logging.FileHandler(
-        filename=file, mode='a', encoding='utf-8')
-    handler.setFormatter(formatter)
-
-    logger = logging.getLogger('Messages')
-    logger.setLevel(logLevel)
-    logger.addHandler(handler)
-    return logger
+    return _logger('Messages', logLevel, file)
